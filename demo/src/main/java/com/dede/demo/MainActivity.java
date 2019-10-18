10f4cc1d20fcf03d7e0e871f6beec1bc7639e651
胡晟昊 <863116814@qq.com>
2019-10-18 14:44:10 +0800

    将walle替换为本地依赖

--- demo/src/main/java/com/dede/demo/MainActivity.java
+++ demo/src/main/java/com/dede/demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.dede.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.meituan.android.walle.PayloadReader;
+import com.meituan.android.walle.WalleChannelReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvChannel = findViewById(R.id.tv_channel);
+        tvChannel.setText(WalleChannelReader.getChannel(this));
     }
 }
